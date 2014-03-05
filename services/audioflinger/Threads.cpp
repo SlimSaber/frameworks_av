fd0898ae9a26244caa3def847554a36d0106a04f
Haynes Mathew George <hgeorge@codeaurora.org>
2014-03-04 17:46:40 -0800

    audioflinger: Fix cleanup of asynccallback thread
    AsyncCallbackThread will not be spawned if the output
    profile does not support non-blocking output or if the HAL does not
    support set_callback API.
    
    Add a check for validity of callback thread object before deleting it.
    
    Change-Id: I3943f80abba3fd7f18fbfd6190bb884f773772b5

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -4106,7 +4106,9 @@ void AudioFlinger::OffloadThread::threadLoop_exit()
         mMixerStatus = MIXER_DRAIN_ALL;
         threadLoop_drain();
     }
-    mCallbackThread->exit();
+    if (mCallbackThread != NULL) {
+        mCallbackThread->exit();
+    }
     PlaybackThread::threadLoop_exit();
 }
 
